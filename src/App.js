79cf2a7652532675af86a05f18ff981c8e3648e2
thesarikonda <140501081+thesarikonda@users.noreply.github.com>
2025-10-15 20:56:56 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,30 @@
-const { Component } = require("react");
-
+import React, { Component } from "react";
 
 class Counter extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      count : 0
-    }
+      count: 0,
+    };
   }
 
-  increment = () =>{
-    this.setState({
-      count : this.state.count + 1
-    })
-  }
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
 
-  decrement = () =>{
-    this.setState({
-      count : this.state.count - 1
-    })
-  }
+  decrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
 
-  render(){
-    return(
-      <div style={{textAlign:'center',marginTop:'50px'}}>
-        <h1>Counter</h1>
-        <h2>{this.state.count}</h2>
+  render() {
+    return (
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <h1>Counter App</h1>
+        <h2>Count: {this.state.count}</h2>
         <button onClick={this.increment}>Increment</button>
         <button onClick={this.decrement}>Decrement</button>
       </div>
-    )
+    );
   }
 }
 
